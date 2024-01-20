25ee4a4ac9f9f7ccd46469daec20ab917a4b03b5
alekseisavinok6 <prueba@correo.com>
2024-01-20 23:55:23 +0100

    practica01_v01_3

--- Main.java
+++ Main.java
@@ -12,16 +12,17 @@ public class Main {
 
 		//declaracion de variables
 		Scanner scanner=new Scanner(System.in).useLocale(Locale.US);
-		Persona persona1;
+		Persona[] personas=new Persona[10];
+
 		String nombre;
 		int edad;
 		double altura;
 		double peso;
-		double imc;
+		
 		
 		//formulario de persona
 		
-		System.out.println("Formulario de Persona");
+		/*System.out.println("Formulario de Persona");
 		System.out.println("=====================");
 		System.out.println();
 		System.out.print("Nombre: ");
@@ -31,13 +32,27 @@ public class Main {
 		System.out.print("Altura: ");
 		altura=scanner.nextDouble();
 		System.out.println("Peso: ");
-		peso=scanner.nextDouble();
+		peso=scanner.nextDouble();*/
 		//fin de formulario de persona
-		
-		persona1=new Persona(nombre, edad, altura, peso);
+
+		//crear objeto persona1
+		//persona1=new Persona(nombre, edad, altura, peso);
+		personas[0]=new Persona("Juan1", 25, 1.75, 80);
+		personas[1]=new Persona("Juan", 25, 1.75, 80);
+		personas[2]=new Persona("María", 30, 1.65, 60);
+		personas[3]=new Persona("Pedro", 40, 1.80, 90);
+		personas[4]=new Persona("Pedro1", 40, 1.80, 90);
+		personas[5]=new Persona("Pedro2", 40, 1.80, 90);
+		personas[6]=new Persona("Pedro3", 40, 1.80, 90);
+		personas[7]=new Persona("Pedro4", 40, 1.80, 90);
+		personas[8]=new Persona("Pedro5", 40, 1.80, 90);
+		personas[9]=new Persona("Pedro6", 40, 1.80, 90);
+
 		
 		//salida de datos
-		System.out.println(persona1.toSaludo());
+		for(int i=0; i<personas.length; i++) {
+			System.out.println(personas[i].toSaludo());
+		}
 
     }
     
--- Persona.java
+++ Persona.java
@@ -1,4 +1,5 @@
 public class Persona {
+	
     private String nombre;
 	private int edad;
 	private double altura;
