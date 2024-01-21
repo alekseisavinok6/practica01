175446043aae153fb3b525c72f8d70a14731187a
alekseisavinok6 <prueba@correo.com>
2024-01-21 12:10:44 +0100

    practica01_v01_4

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 /*
 Autor: Aleksei Savinok
-Fecha: 17/01/24
+Fecha: 18/01/24
 Objetivo: Calcular el IMC de una persona
 */
 
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -12,17 +13,17 @@ public class Main {
 
 		//declaracion de variables
 		Scanner scanner=new Scanner(System.in).useLocale(Locale.US);
-		Persona[] personas=new Persona[10];
+		//Persona[] personas=new Persona[10];
+		ArrayList<Persona> personas=new ArrayList<Persona>();
 
 		String nombre;
 		int edad;
 		double altura;
 		double peso;
 		
-		
 		//formulario de persona
 		
-		/*System.out.println("Formulario de Persona");
+		System.out.println("Formulario de Persona");
 		System.out.println("=====================");
 		System.out.println();
 		System.out.print("Nombre: ");
@@ -32,28 +33,36 @@ public class Main {
 		System.out.print("Altura: ");
 		altura=scanner.nextDouble();
 		System.out.println("Peso: ");
-		peso=scanner.nextDouble();*/
+		peso=scanner.nextDouble();
 		//fin de formulario de persona
 
 		//crear objeto persona1
 		//persona1=new Persona(nombre, edad, altura, peso);
-		personas[0]=new Persona("Juan1", 25, 1.75, 80);
-		personas[1]=new Persona("Juan", 25, 1.75, 80);
-		personas[2]=new Persona("María", 30, 1.65, 60);
-		personas[3]=new Persona("Pedro", 40, 1.80, 90);
-		personas[4]=new Persona("Pedro1", 40, 1.80, 90);
-		personas[5]=new Persona("Pedro2", 40, 1.80, 90);
-		personas[6]=new Persona("Pedro3", 40, 1.80, 90);
-		personas[7]=new Persona("Pedro4", 40, 1.80, 90);
-		personas[8]=new Persona("Pedro5", 40, 1.80, 90);
-		personas[9]=new Persona("Pedro6", 40, 1.80, 90);
+
+		personas.add(new Persona(nombre, edad, altura, peso));
+		
+		personas.add(new Persona("Juan1", 25, 1.75, 83));
+		personas.add(new Persona("Juan", 25, 1.75, 80));
+		personas.add(new Persona("María", 30, 1.65, 61));
+		personas.add(new Persona("Pedro", 42, 1.70, 90));
+		personas.add(new Persona("Pedro1", 47, 1.90, 80));
+		personas.add(new Persona("Pedro2", 33, 1.60, 95));
+		personas.add(new Persona("Pedro3", 30, 1.70, 93));
+		personas.add(new Persona("Pedro4", 37, 1.50, 66));
+
+		//salida de datos
+
+		/*for(Persona p:personas) {
+			System.out.println(p.toSaludo());
+		}*/
+
+		for(int i=0; i<personas.size(); i++) {
+			System.out.println(personas.get(i).toSaludo());
+		}
+    }
+}
+
+
 
 		
-		//salida de datos
-		for(int i=0; i<personas.length; i++) {
-			System.out.println(personas[i].toSaludo());
-		}
 
-    }
-    
-}
